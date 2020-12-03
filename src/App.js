6e4e8cf68cacc7a167ecd079a65c95187340d2e0
Mulk <mabdulul@gmail.com>
2020-12-02 21:06:02 -0500

    test

--- src/App.js
+++ src/App.js
@@ -92,9 +92,7 @@ function App() {
 						{!!state.error}
 						<div className='Job-div'>
 							{state.jobs.length === 0 && state.loading === false ? (
-								<p className='test' style={{ color: theme.syntaxt }}>
-									No jobs found
-								</p>
+								<p style={{ color: theme.syntaxt }}>No jobs found</p>
 							) : (
 								<>
 									<>
